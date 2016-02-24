b5d0c75d661b6c96f999b998b0f5b748f9682a5e
NT <nickt444@gmail.com>
2016-02-24 18:56:56 -0500

    demo test results

--- client/app/demoRecord/demoRecord.controller.js
+++ client/app/demoRecord/demoRecord.controller.js
@@ -1,8 +1,7 @@
-app.controller('demoRecordCTRL',  function($scope, $timeout, $mdSidenav, $log, Demo, $rootScope, $cookies) {
+app.controller('demoRecordCTRL',  function($scope, $timeout,  $mdSidenav, $log, Demo, $rootScope, $cookies) {
     $scope.tanks = [];
     $scope.tests = [];
     $scope.clicked = false;
-
 
     var demoUser = $cookies.getObject('demoUser');
     console.log(demoUser);
@@ -19,28 +18,28 @@ app.controller('demoRecordCTRL',  function($scope, $timeout, $mdSidenav, $log, D
             $scope.tanks = tanks;
         });
 
-    $scope.time = new Date().getTime();
 
+    $scope.submitForm = function(tank, testResults) {
+        console.log(typeof tank);
 
-
-    $scope.submitForm = function(tank, testsResults) {
-        $scope.clicked = true;
-        testsResults.forEach(function (testResult) {
-            Demo.addToFire(tank, testResult.type, testResult.value, $scope.time, $rootScope.user.uid);
-        });
-
-        Demo.recordTime($scope.time,$rootScope.user.uid, tank);
-
-        $rootScope.userCredentials.feed.push({subCat:"watertest", category:"watertests",action:"create", value: tank.$id, detail:"New Test Results Recorded", date: $scope.time});
-        $rootScope.userCredentials.$save();
-
-        $mdToast.show(
-            $mdToast.simple()
-                .content('Enjoy the Demo!')
-                .position('top right')
-                .theme("success-toast")
-                .hideDelay(3000)
-        );
+        //$scope.time = new Date().getTime();
+        //$scope.clicked = true;
+        //testsResults.forEach(function (testResult) {
+        //    Demo.addToFire(tank, testResult.type, testResult.value, $scope.time, demoUser.uid);
+        //});
+        //
+        //Demo.recordTime($scope.time,$rootScope.user.uid, tank);
+        //
+        ////$rootScope.userCredentials.feed.push({subCat:"watertest", category:"watertests",action:"create", value: tank.$id, detail:"New Test Results Recorded", date: $scope.time});
+        ////$rootScope.userCredentials.$save();
+        //
+        //$mdToast.show(
+        //    $mdToast.simple()
+        //        .content('Test Successfully Submitted!')
+        //        .position('top right')
+        //        .theme("success-toast")
+        //        .hideDelay(3000)
+        //);
 
 
         //$cordovaToast.show("New Report Submitted", 'long', 'top').then(function(success) {
@@ -51,7 +50,6 @@ app.controller('demoRecordCTRL',  function($scope, $timeout, $mdSidenav, $log, D
         //    $scope.clicked = false;
         //});
 
-        $ionicHistory.goBack();
     };
 
 
